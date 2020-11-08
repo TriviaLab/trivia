4d2be10ff8be6a0dac59e2e9eb47a6f08bc55a01
TriviaLab <trivialab@gmail.com>
2020-11-08 18:32:28 -0500

    fixc

--- countries.py
+++ countries.py
@@ -1,7 +1,7 @@
 import json
 
-with open('/data/data/com.termux/files/home/trivia/states.json', 'r') as f:
-    states_dict = json.load(f)
+with open('countries.json', 'r') as f:
+    countries_dict = json.load(f)
 
-for states in states_dict:
-    print("Name: " + states['name'])
+for countries in countries_dict:
+    print("Name: " + countries['name'])
