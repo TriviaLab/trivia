043de5814cfbfe35616b4f0027bd79752476782a
TriviaLab <trivialab@gmail.com>
2020-11-08 18:01:56 -0500

    removed states2

--- states2.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('/data/data/com.termux/files/home/trivia/states.json', 'r') as f:
-    states_dict = json.load(f)
-
-for states in states_dict:
-    print("Name: " + states['name'])
