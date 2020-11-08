e374056200401cbd7080cdb7e52eca3ca43c5d55
TriviaLab <trivialab@gmail.com>
2020-11-08 18:40:44 -0500

    Fixed countries.py

--- countries.py
+++ countries.py
@@ -4,4 +4,4 @@ with open('countries.json', 'r') as f:
     countries_dict = json.load(f)
 
 for countries in countries_dict:
-    print("Name: " + countries['name'])
+    print("Name: " + countries['country'])
