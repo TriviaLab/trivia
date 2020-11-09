36bd8aeef7d487793a5ab06aa8afd622a09bcb61
TriviaLab <trivialab@gmail.com>
2020-11-08 19:51:00 -0500

    Fixed stuff

--- trivia.py
+++ trivia.py
@@ -4,10 +4,12 @@ with open('countries.json', 'r') as f:
     countries_dict = json.load(f)
 
 for countries in countries_dict:
-    print("Name: " + countries['country'])
+    print("Country: " + countries['country'] + "     City: " + countries['city'])
+
+print('************************************************************************************************************')
 
 with open('states.json', 'r') as f:
     states_dict = json.load(f)
 
 for states in states_dict:
-    print("Name: " + states['name'])
+    print("State: " + states['name'] + "     City: " + states['capital'])
