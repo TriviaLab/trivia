6ac55c1dfd05c980aca22b0d3d04e6e08ec9a48b
TriviaLab <70901022+TriviaLab@users.noreply.github.com>
2020-11-09 09:53:13 -0500

    Add files via upload

--- trivia.py
+++ trivia.py
@@ -1,18 +1,22 @@
 import json
 import random
 
-random_int = random.randint(1, 2)
+random_int = random.randint(0, 1)
+
+if random_int == 0:
+
+    def random_country():
+        with open('countries.json', 'r') as f:
+            countries_dict = json.load(f)
+            random_index = random.randint(0, len(countries_dict)-1)
+            return countries_dict[random_index]['country']
+            
+    print("Country: " + random_country())
+    print("Country: " + random_country())
+
 if random_int == 1:
-
-    with open('countries.json', 'r') as f:
-        countries_dict = json.load(f)
-
-    for countries in countries_dict:
-        print("Country: " + countries['country'] + "     City: " + countries['city'])
-
-if random_int == 2:
     with open('states.json', 'r') as f:
         states_dict = json.load(f)
 
     for states in states_dict:
-        print("State: " + states['name'] + "     City: " + states['capital'])
+        print("State: " + states['name'] + "     Capital: " + states['capital'])
