704ac718e28b4f0870db2d5ca9103edf0a90ea16
TriviaLab <70901022+TriviaLab@users.noreply.github.com>
2020-11-11 16:17:25 -0500

    Add files via upload

--- trivia.py
+++ trivia.py
@@ -6,7 +6,6 @@ with open("states.json", "r") as read_file:
     
     random_index = random.randint(0, len(states_dict['states'])-1)
 
-    print(random_index)
-
-    print("State: ", states_dict["states"][random_index]["name"])
-    print("Capital: ", states_dict["states"][random_index]["capital"])
+    print("Which city is the capital of " + states_dict["states"][random_index]["name"] + "?")	
+    input()
+    print(states_dict["states"][random_index]["capital"])
