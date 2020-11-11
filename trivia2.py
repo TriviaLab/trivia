3ed0755db1cf94a9924c6972537e0420c373fc81
TriviaLab <70901022+TriviaLab@users.noreply.github.com>
2020-11-11 16:12:08 -0500

    Delete trivia2.py

--- trivia2.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("states.json", "r") as read_file:
-    states_dict = json.load(read_file)
-    
-    print("State: ", states_dict["states"][0]["name"])
-    print("Capital: ", states_dict["states"][0]["capital"])
